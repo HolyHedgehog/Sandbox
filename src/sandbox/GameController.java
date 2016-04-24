df2364a0e9457d034236864fd98218d9bc0793fd
HolyHedgehog <kasynov@gmail.com>
2016-04-24 18:15:06 +0800

    добавлен recurseMove

---
+++ src/sandbox/GameController.java
@@ -0,0 +1,344 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sandbox;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Hedgehog
+ */
+public class GameController {
+
+    private final int[][] CellsColors;
+    private final int[][] CellsPlayer;
+    private static int currentDif = Settings.min_hard.get();
+    private static int cellssize;
+
+    public GameController() {
+        CellsColors = new int[currentDif][currentDif];
+        CellsPlayer = new int[currentDif][currentDif];
+        Random rand = new Random();
+        for (int i = 0; i < currentDif; i++) {
+            for (int j = 0; j < currentDif; j++) {
+                CellsColors[i][j] = rand.nextInt(6);
+                CellsPlayer[i][j] = 0;
+            }
+        }
+        if (CellsColors[0][0] == CellsColors[currentDif - 1][currentDif - 1]) {
+            CellsColors[currentDif - 1][currentDif - 1] = 1;
+            CellsColors[0][0] = 2;
+        }
+
+        CellsPlayer[0][0] = 1;
+        CellsPlayer[currentDif - 1][currentDif - 1] = 2;
+//        this.Move(CellsColors[0][0]);
+//        this.MoveComp(CellsColors[currentDif - 1][currentDif - 1]);
+        recursMove(CellsColors[0][0], 1, 0, 0);
+        recursMove(CellsColors[currentDif - 1][currentDif - 1], 2, currentDif - 1, currentDif - 1);
+        fieldcleaner();
+    }
+
+    public int[][] getCellsColors() {
+        return CellsColors;
+    }
+
+    public int[] getGameStatus() {
+        int[] result = new int[3];
+        int a = 0, b = 0, c = 0;
+        for (int i = 0; i < currentDif; i++) {
+            for (int j = 0; j < currentDif; j++) {
+                switch (CellsPlayer[i][j]) {
+                    case 0: {
+                        c++;
+                        break;
+                    }
+                    case 1: {
+                        a++;
+                        break;
+                    }
+                    case 2: {
+                        b++;
+                        break;
+                    }
+                    default: {
+                        System.out.println("этого быть недолжно. метод геймстатус. проверяй балбес");
+                    }
+                }
+            }
+            result[0] = a;
+            result[1] = b;
+            result[2] = c;
+        }
+        return result;
+    }
+
+//    public void Move(int a) {
+//        boolean flag = true;
+//        while (flag) {
+//            flag = false;
+//            for (int i = 0; i < currentDif; i++) {
+//                for (int j = 0; j < currentDif; j++) {
+//                    if (CellsPlayer[i][j] == 1) {
+//                        CellsColors[i][j] = a;
+//
+//                        if ((j < currentDif - 1) && (CellsPlayer[i][j + 1] == 0) && (CellsColors[i][j + 1] == a)) {
+//                            CellsPlayer[i][j + 1] = 1;
+//                            flag = true;
+//                        }
+//                        if ((i < currentDif - 1) && (CellsPlayer[i + 1][j] == 0) && (CellsColors[i + 1][j] == a)) {
+//                            CellsPlayer[i + 1][j] = 1;
+//                            flag = true;
+//                        }
+//                        if ((i > 0) && (CellsPlayer[i - 1][j] == 0) && (CellsColors[i - 1][j] == a)) {
+//                            CellsPlayer[i - 1][j] = 1;
+//                            flag = true;
+//                        }
+//                        if ((j > 0) && (CellsPlayer[i][j - 1] == 0) && (CellsColors[i][j - 1] == a)) {
+//                            CellsPlayer[i][j - 1] = 1;
+//                            flag = true;
+//                        }
+//                    }
+//                }
+//            }
+//       }
+//    }
+//
+//    public void MoveComp() {
+//        int a = goodMove();
+//        boolean flag = true;
+//        while (flag) {
+//            flag = false;
+//            for (int i = currentDif - 1; i != -1; i--) {
+//                for (int j = currentDif - 1; j != -1; j--) {
+//                    if (CellsPlayer[i][j] == 2) {
+//                        CellsColors[i][j] = a;
+//
+//                        if ((j < currentDif - 1) && (CellsPlayer[i][j + 1] == 0) && (CellsColors[i][j + 1] == a)) {
+//                            CellsPlayer[i][j + 1] = 2;
+//                            flag = true;
+//                        }
+//                        if ((i < currentDif - 1) && (CellsPlayer[i + 1][j] == 0) && (CellsColors[i + 1][j] == a)) {
+//                            CellsPlayer[i + 1][j] = 2;
+//                            flag = true;
+//                        }
+//                        if ((i > 0) && (CellsPlayer[i - 1][j] == 0) && (CellsColors[i - 1][j] == a)) {
+//                            CellsPlayer[i - 1][j] = 2;
+//                            flag = true;
+//                        }
+//                        if ((j > 0) && (CellsPlayer[i][j - 1] == 0) && (CellsColors[i][j - 1] == a)) {
+//                            CellsPlayer[i][j - 1] = 2;
+//                            flag = true;
+//                        }
+//                    }
+//                }
+//            }
+//       }
+//    }
+//
+//    private void MoveComp(int a) {
+//        boolean flag = true;
+//        while (flag) {
+//            flag = false;
+//            for (int i = currentDif - 1; i != -1; i--) {
+//                for (int j = currentDif - 1; j != -1; j--) {
+//                    if (CellsPlayer[i][j] == 2) {
+//                        CellsColors[i][j] = a;
+//
+//                        if ((j < currentDif - 1) && (CellsPlayer[i][j + 1] == 0) && (CellsColors[i][j + 1] == a)) {
+//                            CellsPlayer[i][j + 1] = 2;
+//                            flag = true;
+//                        }
+//                        if ((i < currentDif - 1) && (CellsPlayer[i + 1][j] == 0) && (CellsColors[i + 1][j] == a)) {
+//                            CellsPlayer[i + 1][j] = 2;
+//                            flag = true;
+//                        }
+//                        if ((i > 0) && (CellsPlayer[i - 1][j] == 0) && (CellsColors[i - 1][j] == a)) {
+//                            CellsPlayer[i - 1][j] = 2;
+//                            flag = true;
+//                        }
+//                        if ((j > 0) && (CellsPlayer[i][j - 1] == 0) && (CellsColors[i][j - 1] == a)) {
+//                            CellsPlayer[i][j - 1] = 2;
+//                            flag = true;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+    
+    public int goodMove() {
+
+        int[] Count_colors_cells = new int[6];
+
+        int[][] buffCellsColors = new int[40][40];
+        int[][] buffCellsPlayer = new int[40][40];
+
+        for (int k = 0; k < 6; k++) {
+
+            Count_colors_cells[k] = 0;
+
+            for (int i = 0; i < currentDif; i++) {                                      //Натерпелся с этим место. Изначально было так
+                for (int j = 0; j < currentDif; j++) {                                  //buffCellsColors=getCellsColors();
+                    buffCellsColors[i][j] = CellsColors[i][j];                  //Долго думал почему CellsColors меняется.
+                    buffCellsPlayer[i][j] = CellsPlayer[i][j];                  //СОздавалась не копия а ссылка. так что теперь вот так.
+                }
+
+            }
+
+            if ((buffCellsColors[0][0] != k) && (buffCellsColors[currentDif - 1][currentDif - 1] != k)) {
+
+                boolean flag = true;
+                while (flag) {
+                    flag = false;
+                    for (int i = currentDif - 1; i != -1; i--) {
+                        for (int j = currentDif - 1; j != -1; j--) {
+                            if (buffCellsPlayer[i][j] == 2) {
+                                buffCellsColors[i][j] = k;
+
+                                if ((j < currentDif - 1) && (buffCellsPlayer[i][j + 1] == 0) && (buffCellsColors[i][j + 1] == k)) {
+                                    buffCellsPlayer[i][j + 1] = 2;
+                                    Count_colors_cells[k]++;
+                                    flag = true;
+                                }
+                                if ((i < currentDif - 1) && (buffCellsPlayer[i + 1][j] == 0) && (buffCellsColors[i + 1][j] == k)) {
+                                    buffCellsPlayer[i + 1][j] = 2;
+                                    Count_colors_cells[k]++;
+                                    flag = true;
+                                }
+                                if ((i > 0) && (buffCellsPlayer[i - 1][j] == 0) && (buffCellsColors[i - 1][j] == k)) {
+                                    buffCellsPlayer[i - 1][j] = 2;
+                                    Count_colors_cells[k]++;
+                                    flag = true;
+                                }
+                                if ((j > 0) && (buffCellsPlayer[i][j - 1] == 0) && (buffCellsColors[i][j - 1] == k)) {
+                                    buffCellsPlayer[i][j - 1] = 2;
+                                    Count_colors_cells[k]++;
+                                    flag = true;
+                                }
+
+                            }
+
+                        }
+
+                    }
+                }
+            }
+        }
+        /*Это кусок использовался для отслеживания правильности работы метода*/
+//        System.out.println("\n" + "ATENTION");
+//        for (int i = 0; i < 6; i++) {
+//            System.out.print("   " + Count_colors_cells[i]);
+//        }
+        int max_val = Integer.MIN_VALUE, index_val = 0, sum = 0;
+        for (int i = 0; i < 6; i++) {
+            sum = sum + Count_colors_cells[i];
+            if (Count_colors_cells[i] > max_val) {
+                max_val = Count_colors_cells[i];
+                index_val = i;
+            }
+        }
+        if (sum == 0) {
+            index_val = genRandColor();
+        }
+//        System.out.println("returned int - "+index_val);
+        return index_val;
+    }
+
+    private int genRandColor() {
+        Random rand = new Random();
+        int result = rand.nextInt(6);
+        while ((result == CellsColors[0][0]) || (result == CellsColors[currentDif - 1][currentDif - 1])) {
+            result = rand.nextInt(6);
+        }
+        return result;
+
+    }
+
+    public static int getCurrentDif() {
+        return currentDif;
+    }
+
+    public static void setCurrentDif(int currentDif) {
+        GameController.currentDif = currentDif;
+        if (currentDif == Settings.max_hard.get()) {
+            cellssize = 10;
+        }
+        if (currentDif == Settings.med_hard.get()) {
+            cellssize = 20;
+        }
+        if (currentDif == Settings.min_hard.get()) {
+            cellssize = 40;
+        }
+    }
+
+    public static int getCellssize() {
+        return cellssize;
+    }
+
+    public void dispose() {
+        for (int i = 0; i < currentDif; i++) {
+            for (int j = 0; j < currentDif; j++) {
+                CellsColors[i][j] = 0;
+                CellsPlayer[i][j] = 0;
+
+            }
+        }
+
+    }
+
+    public void fieldcleaner() {
+        for (int i = 0; i < currentDif; i++) {
+            for (int j = 0; j < currentDif; j++) {
+                if ((CellsPlayer[i][j] == 3)) {
+                    CellsPlayer[i][j] = CellsPlayer[i][j] - 2;
+                }
+                if ((CellsPlayer[i][j] == 4)) {
+                    CellsPlayer[i][j] = CellsPlayer[i][j] - 2;
+                }
+            }
+        }
+    }
+
+    public void recursMove(int a, int b, int i, int j) {
+        if (a == -1) {
+            a = goodMove();
+        }
+
+        if (CellsPlayer[i][j] == b) {
+            CellsPlayer[i][j] = b + 2;
+            CellsColors[i][j] = a;
+        }
+        if ((j  < currentDif -1) && (CellsPlayer[i][j + 1] == 0) && (CellsColors[i][j + 1] == a)) {
+            CellsPlayer[i][j + 1] = b;
+            recursMove(a, b, i, j + 1);
+        } else if ((j  < currentDif -1) && (CellsPlayer[i][j + 1] == b)) {
+            recursMove(a, b, i, j + 1);
+        }
+
+        if ((i  < currentDif-1 ) && (CellsPlayer[i + 1][j] == 0) && (CellsColors[i + 1][j] == a)) {
+            CellsPlayer[i + 1][j] = b;
+            recursMove(a, b, i + 1, j);
+        } else if ((i  < currentDif-1 ) && (CellsPlayer[i + 1][j] == b)) {
+            recursMove(a, b, i + 1, j);
+        }
+
+        if ((j  >0) && (CellsPlayer[i][j - 1] == 0) && (CellsColors[i][j - 1] == a)) {
+            CellsPlayer[i][j - 1] = b;
+            recursMove(a, b, i, j - 1);
+        } else if ((j  > 0) && (CellsPlayer[i][j - 1] == b)) {
+            recursMove(a, b, i, j - 1);
+        }
+
+        if ((i  > 0) && (CellsPlayer[i - 1][j] == 0) && (CellsColors[i - 1][j] == a)) {
+            CellsPlayer[i - 1][j] = b;
+            recursMove(a, b, i - 1, j);
+        } else if ((i >0) && (CellsPlayer[i - 1][j] == b)) {
+            recursMove(a, b, i - 1, j);
+        }
+    }
+
+}
